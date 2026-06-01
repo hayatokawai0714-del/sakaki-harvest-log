5cfe3dde6336039a1efbcfa4c661488664329372
hayatokawai0714-del <hayatokawai0714@gmail.com>
2026-06-01 21:43:10 +0900

    chore: set default Apps Script endpoint

--- docs/app.js
+++ docs/app.js
@@ -4,7 +4,7 @@
 
   // Apps Script WebアプリURL（あとで差し替え）
   // 例: const GAS_ENDPOINT = 'https://script.google.com/macros/s/XXXX/exec';
-  const GAS_ENDPOINT = "https://script.google.com/macros/s/REPLACE_ME/exec";
+  const GAS_ENDPOINT = "＜ここにApps Script WebアプリURLを貼る＞";
 
   const STORAGE_KEY = "sakakiHarvestLog.v2";
   const SETTINGS_KEY = "sakakiHarvestLog.settings.v1";
@@ -800,6 +800,7 @@
 
   init();
 })();
+
 
 
 
